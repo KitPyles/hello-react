9c04cfeffa8a73e7654be5dcf3fb28d2a89c8c53
KitPyles <pyles.kit@gmail.com>
2023-05-13 06:55:40 -0500

    Completed through Section 6 (Project 2)

---
+++ section5/objects.js
@@ -0,0 +1,35 @@
+let humanBeing = {
+    name: "Kit",
+    age: 40,
+    alive: true,
+    maritalStatus: "married"
+}
+
+let pizza = {}
+pizza.crust = "hand-tossed";
+pizza.sauce = "alfredo";
+pizza.cheese = "mozzarella, parmesan";
+pizza.meat = "chicken, bacon";
+
+let cup = {}
+cup['ounces'] = 12;
+cup['color'] = 'purple';
+cup['clean'] = false;
+
+let snowman = {
+    isSnow: true,
+    color: 'white',
+    madeOfSnowballs: 3,
+    'first name': 'Frosty'
+}
+
+// console.log(`${snowman['first name']} | ${snowman.isSnow} | ${snowman.madeOfSnowballs} | ${snowman.color}`)
+
+let house = {}
+house.sqft = 2000;
+house.color = "white";
+house.hasGarage = true;
+house.occupied = true;
+house['street address'] = '5123 Rinker Rd';
+house['city state & zipcode'] = 'Kansas City, MO 63129';
+house['number of bedrooms'] = 3;
